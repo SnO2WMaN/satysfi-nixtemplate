8ae8d01291d416deedc64bef7f7ab13664725a67
SnO2WMaN <me@sno2wman.net>
2022-06-20 19:20:08 +0900

    setup env

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+    satyxin.url = "github:SnO2WMaN/satyxin";
+    satysfi-tools.url = "github:SnO2WMaN/satysfi-tools-nix";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    devshell,
+    satyxin,
+    satysfi-tools,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlay
+            satyxin.overlay
+            satysfi-tools.overlay
+          ];
+        };
+      in {
+        devShell = pkgs.devshell.mkShell {
+          imports = [
+            (pkgs.devshell.importTOML ./devshell.toml)
+          ];
+        };
+      }
+    );
+}
